da4e868a2e5330b65ce50d317000ea64a320c932
FathiBanki <fathibankii@gmail.com>
2026-02-10 16:32:42 +0300

    removed hero section from home page according to gatcha suggestion

--- Client/app/page.jsx
+++ Client/app/page.jsx
@@ -164,7 +164,7 @@ export default function Home() {
     <>
       <main>
         {/* Hero Section */}
-        <section className="relative py-20 md:py-32 bg-gradient-nature">
+        {/*  <section className="relative py-20 md:py-32 bg-gradient-nature">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
               <div className="animate-fade-up">
@@ -195,7 +195,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         {/* Featured Places */}
         <section className="py-12 md:py-24 bg-background">
@@ -222,16 +222,16 @@ export default function Home() {
                       >
                         <div
                           className="
-              w-full
-              px-5 py-3
-              rounded-full
-              text-center
-              bg-card
-              hover:bg-primary/5
-              transition
-              font-medium
-              text-foreground
-            "
+                                    w-full
+                                    px-5 py-3
+                                    rounded-full
+                                    text-center
+                                    bg-card
+                                    hover:bg-primary/5
+                                    transition
+                                    font-medium
+                                    text-foreground
+                                  "
                         >
                           {type.name}
                         </div>
